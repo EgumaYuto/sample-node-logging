2238eadbcd22140990dc91920ddd448b8f79cf48
yuto.eguma <yuto.eguma@gmail.com>
2021-12-29 22:42:50 +0900

    色々別の情報も出してみる

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { MyLogger } from "./common/MyLogger"
 
 const logger = MyLogger.create();
-logger.debug("debug")
-logger.info("info")
-logger.warning("warning")
-logger.error("error")
-logger.critical("critical")
+logger.debug("debug", "debug args")
+logger.info("info", "info args")
+logger.warning("warning", "warning args")
+logger.error("error", new Error("sample error"))
+logger.critical("critical", new Error("sample critical error"))
