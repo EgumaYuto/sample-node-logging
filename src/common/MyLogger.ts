4091091a0464fb53d75c9cd62c27108efcde3947
yuto.eguma <yuto.eguma@gmail.com>
2021-12-30 00:37:53 +0900

    Stack trace の表示

--- src/common/MyLogger.ts
+++ src/common/MyLogger.ts
@@ -1,34 +1,71 @@
-import { Logger, getLogger } from "log4js";
+import { Logger, getLogger, configure, addLayout } from "log4js";
 
 export class MyLogger {
   private logger: Logger;
-  private constructor() {
+  private constructor(fileName: string) {
     // TODO log4jsの設定はドキュメントを読む
     // log4js.configure({});
     // https://www.npmjs.com/package/log4js
+    addLayout('json', (config) => (logEvent) => {
+      return JSON.stringify({
+        fileName: fileName,
+        severity: logEvent.level.levelStr,
+        message: this.formatMessage(logEvent.data),
+        timestamp: logEvent.startTime
+      }) + config.separator
+    })
+    configure({
+      appenders: {
+        out: {
+          type: 'stdout', layout: {type : 'json', separator: ''}
+        }
+      },
+      categories: {
+        default: { 
+          appenders: ['out'], level: 'all'
+        }
+      }
+    })
     this.logger = getLogger();
-    this.logger.level = "debug"
   }
 
-  public static create = () => new MyLogger();
+  private formatMessage(data: any[]) : string | undefined{
+    if (!data || data.length === 0) {
+      return undefined
+    }
+    let message = ''
+    const separator = ', '
+    for (let index = 0; index < data.length; index++) {
+      if (data[index] instanceof Error) {
+        const d = data[index]
+        message += d.stack
+      } else {
+        message += data[index]
+      }
+      message += separator
+    }
+    return message.substring(0, message.length - separator.length)
+  }
 
-  public debug = (message: any, ...args: any[]): void => {
+  public static create = (fileName: string) => new MyLogger(fileName);
+
+  public debug = (message: string, ...args: any[]): void => {
     this.logger.debug(message, ...args);
   };
 
-  public info = (message: any, ...args: any[]): void => {
+  public info = (message: string, ...args: any[]): void => {
     this.logger.info(message, ...args);
   };
 
-  public warning = (message: any, ...args: any[]): void => {
+  public warning = (message: string, ...args: any[]): void => {
     this.logger.warn(message, ...args);
   };
 
-  public error = (message: any, ...args: any[]): void => {
+  public error = (message: string, ...args: any[]): void => {
     this.logger.error(message, ...args);
   };
 
-  public critical = (message: any, ...args: any[]): void => {
+  public critical = (message: string, ...args: any[]): void => {
     this.logger.fatal(message, ...args);
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { MyLogger } from "./common/MyLogger"
 
-const logger = MyLogger.create();
-logger.debug("debug", "debug args")
-logger.info("info", "info args")
-logger.warning("warning", "warning args")
-logger.error("error", new Error("sample error"))
-logger.critical("critical", new Error("sample critical error"))
+const logger = MyLogger.create("index.ts");
+logger.debug("debug message", "debug args")
+logger.info("info message", "info args")
+logger.warning("warning message", "warning args")
+logger.error("error message", new Error("sample error"))
+logger.critical("critical message", new Error("sample critical error"))
